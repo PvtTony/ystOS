9aed9ca48ec86a6936009f840989d7bce28c00a9
PvtTony <yusongtao518@outlook.com>
2020-04-28 22:38:21 +0800

    chapter 6 fininshed (GDT)

--- init/entry.c
+++ init/entry.c
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "console.h"
 #include "debug.h"
+#include "gdt.h"
 
 /*
 ##    ##  ######  ########  #######   ######  
@@ -10,22 +11,35 @@
    ##          ##    ##    ##     ##       ## 
    ##    ##    ##    ##    ##     ## ##    ## 
    ##     ######     ##     #######   ######  
+
+   System Entry
 */
+
+static void print_logo();
 
 int kern_entry()
 {
     console_clear();
+    print_logo();
     init_debug();
-    console_write_color("Hello, ystOS Kernel!\n", rc_black, rc_green);
-    console_write("\n");
-    console_write_color("##    ##  ######  ########  #######   ######  \n", rc_black, rc_cyan);
-    console_write_color(" ##  ##  ##    ##    ##    ##     ## ##    ## \n", rc_black, rc_cyan);
-    console_write_color("  ####   ##          ##    ##     ## ##       \n", rc_black, rc_cyan);
-    console_write_color("   ##     ######     ##    ##     ##  ######  \n", rc_black, rc_cyan);
-    console_write_color("   ##          ##    ##    ##     ##       ## \n", rc_black, rc_cyan);
-    console_write_color("   ##    ##    ##    ##    ##     ## ##    ## \n", rc_black, rc_cyan);
-    console_write_color("   ##     ######     ##     #######   ######  \n", rc_black, rc_cyan);
+    init_gdt();
     panic("test");
     
     return 0;
+}
+
+/*
+ * 打印 logo
+ */
+void print_logo()
+{
+    console_write("\n");
+    console_write_color("##    ##  ######  ########  #######   ######  \n", rc_black, rc_green);
+    console_write_color(" ##  ##  ##    ##    ##    ##     ## ##    ## \n", rc_black, rc_green);
+    console_write_color("  ####   ##          ##    ##     ## ##       \n", rc_black, rc_green);
+    console_write_color("   ##     ######     ##    ##     ##  ######  \n", rc_black, rc_green);
+    console_write_color("   ##          ##    ##    ##     ##       ## \n", rc_black, rc_green);
+    console_write_color("   ##    ##    ##    ##    ##     ## ##    ## \n", rc_black, rc_green);
+    console_write_color("   ##     ######     ##     #######   ######  \n", rc_black, rc_green);
+    console_write("\n");
 }
